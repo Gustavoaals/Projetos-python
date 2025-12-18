5c34d3425e37c1ac2d3aedb388307ed097183fbf
Gustavoaals <gustavoalvess842@gmail.com>
2025-12-18 17:58:40 -0300

    Aluguel de Carro

--- exercicio06.py
+++ exercicio06.py
@@ -6,4 +6,4 @@ num = int(input("Digite um numero: "))
 
 print(f"O dobro de {num} é {num*2} e o triplo {num*3} ")
 print(f"E a Raiz quadrada é {num ** (1/2):.2f}")
-print(f"E a raiz quadrada é {pow(num, (1/2)):.2f}")# outra forma de calcular raiz quadrada
+print(f"E a raiz quadrada é {pow(num, (1/2)):.2f}")# outra forma de calcular

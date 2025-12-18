3b5c6d44d155488dfbb541492dd979457e1ca262
Gustavoaals <gustavoalvess842@gmail.com>
2025-12-18 12:43:08 -0300

    exercicio 09, criando uma tabuada em python

---
+++ exercicio05.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor
+# e seu antecessor
+
+num = int(input("Digite um numero: "))
+
+print(
+    f"O número digitado é {num} o sucessor é {num + 1}\n e seu antecessor é {num - 1} ")

40a84c02fb5862030da2fca92f2bbf86f066811a
Gustavoaals <gustavoalvess842@gmail.com>
2025-12-21 15:12:04 -0300

    exercicio 19

---
+++ exercicio19.py
@@ -0,0 +1,25 @@
+import random
+import time 
+
+print("-" * 35)
+print("SORTEIO QUEM VAI APAGAR O QUADRO")
+print("-" * 35)
+
+aluno1 = str(input("Digite o nome do primeiro aluno: "))
+aluno2 = str(input("Digite o nome do segundo aluno: "))
+aluno3 = str(input("Digite o nome do terceiro aluno: "))
+aluno4 = str(input("Digite o nome do quarto aluno: "))
+
+sorteado = random.choice([aluno1, aluno2, aluno3, aluno4])
+
+
+print("Realizando o sorteio")
+time.sleep(2)
+
+print("E quem vai limpar o quadro é...")
+time.sleep(3)
+
+print (f"Parábens,{sorteado}! você vai limpar o quadro.")
+
+
+
---
+++ modulosRandom.py
@@ -0,0 +1,9 @@
+import emoji
+
+print("ok")
+print(emoji.emojize("Hello :sunglasses:"))
+
+
+#import random
+#um = random.randint(1,10)
+#print(num)

032ffa7af39aca501c1cd4c4e9ef844d290d9e2c
Jose Gustavo <seuemail@gmail.com>
2026-01-17 10:51:34 -0300

    Cores no terminal

---
+++ cores.py
@@ -0,0 +1,10 @@
+print("\033[4;33;46mTeste\033[m")   # sublinhado, amarelo, fundo ciano
+print("\033[1;35;43mTeste\033[m")   # negrito, roxo, fundo amarelo
+print("\033[0;30;42mTeste\033[m")   # preto, fundo verde
+print("\033[7;30mTeste\033[m")      # invertido
+
+print("\033[7;33/44mOlá, mundo!\033[m")
+
+a = 3
+b = 5
+print(f"O valores sao \033[32m{a} e \033[31m{b}")
--- exericicio35.py
+++ exericicio35.py
@@ -15,4 +15,3 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print(f"Os segmentos acima podem formar um triângulos!")
 else:
     print("Os segmentos acima NÃO podem formar triângulo")
-    print(" ")

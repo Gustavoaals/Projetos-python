8f9c187f7add4342d64c95cdcffbfa7471c7c574
Jose Gustavo <seuemail@gmail.com>
2026-01-17 10:09:11 -0300

    atualizações do exercicio35

--- exericicio35.py
+++ exericicio35.py
@@ -5,8 +5,14 @@ print('-=' * 20)
 print("ANALISANDOR DE TRINÂNGULO".center(40))
 print('-=' * 20)
 
-a = int(input("Primeiro segmento: "))
-b = int(input("Segundo segmento: "))
-c = int(input("Terceiro segmento: "))
+r1 = float(input("Primeiro segmento: "))
+r2 = float(input("Segundo segmento: "))
+r3 = float(input("Terceiro segmento: "))
 time.sleep(2)
 print("ANALISANDO...".center(40))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print(f"Os segmentos acima podem formar um triângulos!")
+else:
+    print("Os segmentos acima NÃO podem formar triângulo")
+    print(" ")

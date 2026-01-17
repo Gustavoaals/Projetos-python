6c720520deb6b1ae508d6c759bc42ab18eb03a43
Jose Gustavo <seuemail@gmail.com>
2026-01-17 09:59:11 -0300

    Exercicio 35 criado

---
+++ exericicio35.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se 
+#podem ou não formar um triângulo.
+
+print('-=' * 20)
+print("ANALISANDOR DE TRINÂNGULO".center(40))
+print('-=' * 20)

58a7833ad4bf3b0725888924e8a3ca22ebfe73c9
Jose Gustavo <seuemail@gmail.com>
2026-01-17 10:02:13 -0300

    atualizações do exercicio35

--- exericicio35.py
+++ exericicio35.py
@@ -1,6 +1,12 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se 
 #podem ou não formar um triângulo.
-
+import time
 print('-=' * 20)
 print("ANALISANDOR DE TRINÂNGULO".center(40))
 print('-=' * 20)
+
+a = int(input("Primeiro segmento: "))
+b = int(input("Segundo segmento: "))
+c = int(input("Terceiro segmento: "))
+time.sleep(2)
+print("ANALISANDO...".center(40))
